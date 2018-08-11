2f5b1098ac2085390cb926492919034976076012
Francisco Igor <franciscoigor@gmail.com>
2018-08-11 00:05:08 -0700

    Update mysql-password-reset.sh

--- mysql-password-reset.sh
+++ mysql-password-reset.sh
@@ -6,37 +6,36 @@
 # References: https://help.ubuntu.com/community/MysqlPasswordReset 
 # 
 
+message () {
+  echo -e "\033[0;32m$1\033[0m"
+  sleep 1
+}
+
 export NEWPASSWORD=123456
 
-# Stop the mysql demon process using this command :
+message "Stop the mysql daemon process using this command"
 sudo /etc/init.d/mysql stop
 
-# Start the mysqld demon process using the --skip-grant-tables option with this command 
+message "Start the mysqld daemon process"
 sudo /usr/sbin/mysqld --skip-grant-tables --skip-networking &
 
-# Because you are not checking user privs at this point, it's safest to disable networking. 
-# In Dapper, /usr/bin/mysqld... did not work. However, mysqld --skip-grant-tables did.
+message "Flushing privileges"
+mysql -u root -e "FLUSH PRIVILEGES"
 
-# start the mysql client process using this command 
-# Execute this command to be able to change any password
-mysql -u root -e "SHOW PRIVILEGES"
+message "Reset/update your password" 
+mysql -u root -e "SET PASSWORD FOR root@'localhost' = PASSWORD('$NEWPASSWORD');"
 
-# Then reset/update your password 
-#   SET PASSWORD FOR root@'localhost' = PASSWORD('password');
-# If you have a mysql root account that can connect from everywhere, you should also do:
-#   UPDATE mysql.user SET Password=PASSWORD('newpwd') WHERE User='root';
+message "Reset/update your password - local access" 
+mysql -u root -e "UPDATE mysql.user SET Password = PASSWORD('$NEWPASSWORD') WHERE Host = 'localhost' AND User = 'root';"
 
-# Alternate Method:
-mysql -u root -e "USE mysql; UPDATE user SET Password = PASSWORD('$NEWPASSWORD') WHERE Host = 'localhost' AND User = 'root';"
+message "Reset/update your password - remote access" 
+mysql -u root -e "UPDATE mysql.user SET Password = PASSWORD('$NEWPASSWORD') WHERE Host = '%' AND User = 'root';"
 
-# And if you have a root account that can access from everywhere:
-mysql -u root -e "USE mysql; UPDATE user SET Password = PASSWORD('$NEWPASSWORD') WHERE Host = '%' AND User = 'root';"
+message "Flushing privileges" 
+mysql -u root -e "FLUSH PRIVILEGES"
 
-# For either method, once have received a message indicating a successful query (one or more rows affected), flush privileges:
-mysql -u root -e "SHOW PRIVILEGES"
-
-
-# Then stop the mysqld process and relaunch it with the classical way:
-
+message "Stop the mysqld process"
 sudo /etc/init.d/mysql stop
+
+message "Restart mysql daemon. Please wait..."
 sudo /etc/init.d/mysql start
